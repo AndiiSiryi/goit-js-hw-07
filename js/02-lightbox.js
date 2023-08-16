11c133a68192a5f1992101818de6d85abab9381a
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-08-16 14:53:10 +0300

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems
+function createGalleryItem({preview, original, description}) {
+    const galleryItem = `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    return galleryItem
+}
+const galleryItemsMarkup = galleryItems.map(createGalleryItem).join('');
+galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup)
+
+// Ініціалізація бібліотеки та додавання options captionsData та captionDelay
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'outside',
+    captionDelay: 250,
+});
+
 console.log(galleryItems);
